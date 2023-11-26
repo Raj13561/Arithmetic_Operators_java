f9238cd5934e11296627f4fe8c94295e95c7d706
ADMIN <ADMIN@DESKTOP-P1397RD>
2023-11-26 18:43:47 +0530

    Ussing Arithmetic Operators in Java Language

---
+++ practical2/src/practical2/ArithmeticDemo.java
@@ -0,0 +1,21 @@
+package practical2;
+
+public class ArithmeticDemo {
+
+	public static void main(String[] args) {
+	
+		int result = 1+2;
+	        System.out.println("1+2 = " + result);
+	    int  original_result = result;
+           result = result -1 ;
+                System.out.println(original_result + "-1 = " + result);
+             original_result=result;
+           result = result * 2 ;
+                System.out.println(original_result + "*2 = " + result);
+             original_result=result;
+           result = result / 2 ;
+                System.out.println(original_result + "/2 = " + result);
+	}
+	
+
+}
